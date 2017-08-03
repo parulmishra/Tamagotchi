dc81c8b587adc710155b296ef7d34c2d819e8633
Parul Mishra <parul.mishra78@gmail.com>
2017-08-02 22:19:17 -0700

    need to add isdead logic

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,5 +12,55 @@ namespace Game.Controllers
         {
             return View();
         }
+		
+		[HttpGet("/tamagotchis")]
+        public ActionResult Tamagotchis()
+        {
+            List<Tamagotchi> t = Tamagotchi.GetAllTamagotchi();
+            return View(t);
+        }
+
+        [HttpGet("/tamagotchis/{id}")]
+        public ActionResult GetTamagotchi(int id)
+        {
+            Tamagotchi t = Tamagotchi.Find(id);
+			return View("GetTamagotchi", t);
+        }
+		
+		[HttpPost("/tamagotchi/update/{id}")]
+        public ActionResult UpdateTamagotchi(int id, string Command)
+        {
+            Tamagotchi t = Tamagotchi.Find(id);
+			if(Command == "feed")
+			{
+				t.Feed();
+			}
+			else if(Command == "play")
+			{
+				t.Play();
+			}
+			else if(Command == "sleep")
+			{
+				t.Sleep();
+			}
+			else if(Command == "timepass")
+			{
+				t.Timepass();
+			}
+			else
+			{
+				throw new Exception("Invalid action: " + Command);
+			}
+			
+			return View("GetTamagotchi", t);
+        }
+
+        [HttpPost("/tamagotchi/create")]
+        public ActionResult CreatePlace()
+        {
+            Tamagotchi t = new Tamagotchi(Request.Form["name"], int.Parse(Request.Form["food"]), int.Parse(Request.Form["attention"]), int.Parse(Request.Form["rest"]));
+
+            return View("GetTamagotchi", t);
+        }
     }
 }
--- Models/Tamagotchi.cs
+++ Models/Tamagotchi.cs
@@ -5,25 +5,90 @@ namespace Game.Models
 {
     public class Tamagotchi
     {
-        private int _food = 20;
-        private int _attention = 20;
-        private int _rest = 20;
-        private int _happiness = 20;
-
-        public void feed()
+        private int _food;
+        private int _attention;
+        private int _rest;
+        private int _happiness = 5;
+		private bool _isDead = false;
+		private string _name;
+		private static List<Tamagotchi> _tamagotchis = new List<Tamagotchi>();
+		private int _id;
+		
+		public Tamagotchi(string name, int food, int attention, int rest)
+		{
+			_name = name;
+			_food = food;
+			_attention = attention;
+			_rest = rest;
+			_tamagotchis.Add(this);
+			_id = _tamagotchis.Count;
+		}
+		
+		public int GetId()
+		{
+			return _id;
+		}
+		
+		public static List<Tamagotchi> GetAllTamagotchi()
+		{
+			return _tamagotchis;
+		}
+		public static Tamagotchi Find(int id)
+		{
+			return _tamagotchis[id-1];
+		}
+		public string GetName()
+		{
+			return _name;	
+		}
+		public int GetFood()
+		{
+			return _food;
+		}
+		public int GetAttention()
+		{
+			return _attention;
+		}
+		public int GetHappiness()
+		{
+			return _happiness;
+		}
+		public int GetRest()
+		{
+			return _rest;
+		}
+        public void Feed()
         {
+			if(_isDead == true)
+				return;
             _food += 5;
             _happiness += 5;
         }
-        public void play()
+        public void Play()
         {
+			if(_isDead == true)
+				return;
             _attention += 5;
             _happiness += 5;
         }
-        public void sleep()
+        public void Sleep()
         {
+			if(_isDead == true)
+				return;
             _rest += 5;
-            _attention -= 5;
+            _happiness += 5;
         }
+		public void Timepass()
+		{
+			if(_happiness == 0)
+			{
+				_isDead = true;
+				return;
+			}
+			
+			_happiness -= 5;
+			_food -= 5;
+			_attention -= 5;
+		}
     }
 }
